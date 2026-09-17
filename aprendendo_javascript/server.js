4c6c2fe9ffbe79adf48f7851789013c8ee4a0d00
Tiago santos da silva <tiagosantoss016@gmail.com>
2026-09-17 00:24:29 +0000

    att

--- aprendendo_javascript/server.js
+++ aprendendo_javascript/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 const porta = 3000;
 
 const livros = [
     { id: 1, titulo: "Homem aranha", autor: "Stan Lee", ano: 1999},
-    { id: 2,titulo: "Dom Casmurro", autor: "Machado de Assis", ano: 1875 },
-    { id: 3,titulo: "O Cortiço", autor: "Aluísio Azevedo", ano: 1880 }, 
-    { id: 4,titulo: "Iracema", autor: "José de Alencar", ano: 1777 }
+    { id: 2, titulo: "Dom Casmurro", autor: "Machado de Assis", ano: 1875 },
+    { id: 3, titulo: "O Cortiço", autor: "Aluísio Azevedo", ano: 1880 }, 
+    { id: 4, titulo: "Iracema", autor: "José de Alencar", ano: 1777 }
 ];
 
 app.get('/livros', (req, res) => {
@@ -14,17 +15,22 @@ app.get('/livros', (req, res) => {
 });
 
 app.get('/livros/:id', (req, res) => {
-  let livroEncontrado = null;
-    for(let i = 0; i < livros.length; i = i + 1){
-      if(Number(req.params.id) === livros[i].id){
-        livroEncontrado = livros[i];   
-      }
-    }
-    if(livroEncontrado != null){
+  const livroEncontrado = livros.find((livro) => livro.id === Number(req.params.id));
+    
+  if(livroEncontrado){
       res.json(livroEncontrado);
-    }else{
+  }else{
       res.send("Livro não encontrado");
     }
+});
+
+app.post('/livros', (req, res) =>{
+  
+  let novoLivro = { id: livros.length + 1, titulo: req.body.titulo , autor: req.body.autor , ano: req.body.ano};
+
+  livros.push(novoLivro);
+  res.json(novoLivro);                                                                                                                                                                                                                                                                
+
 });
 
 app.listen(porta, () => {
