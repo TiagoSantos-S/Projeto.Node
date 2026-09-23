cd8ce23dda3b864f9794e27d64874657a2379dce
Tiago santos da silva <tiagosantoss016@gmail.com>
2026-09-23 20:24:19 +0000

    att

--- aprendendo_javascript/db.js
+++ aprendendo_javascript/db.js
@@ -4,7 +4,9 @@ const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: 'senha123',
-  database: 'crud_livros'
+  database: 'crud_livros',
+  charset: 'utf8mb4'
 });
+
 
 module.exports = pool;
--- aprendendo_javascript/server.js
+++ aprendendo_javascript/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+const pool = require('./db');
 const porta = 3000;
 
 const livros = [
@@ -10,26 +11,55 @@ const livros = [
     { id: 4, titulo: "Iracema", autor: "José de Alencar", ano: 1777 }
 ];
 
-app.get('/livros', (req, res) => {
-  res.json(livros);
+app.get('/livros', async (req, res) => {
+  const [linhas] = await pool.query('SELECT * FROM livros');
+  res.json(linhas);
 });
 
-app.get('/livros/:id', (req, res) => {
+app.get('/livros/:id', async (req, res) => {
   const livroEncontrado = livros.find((livro) => livro.id === Number(req.params.id));
     
   if(livroEncontrado){
       res.json(livroEncontrado);
   }else{
-      res.send("Livro não encontrado");
+      res.send("Livro não encontrado\n");
     }
 });
 
-app.post('/livros', (req, res) =>{
+app.post('/livros', async (req, res) =>{
   
   let novoLivro = { id: livros.length + 1, titulo: req.body.titulo , autor: req.body.autor , ano: req.body.ano};
 
   livros.push(novoLivro);
   res.json(novoLivro);                                                                                                                                                                                                                                                                
+
+});
+
+app.put('/livros/:id', async (req, res) => {
+  const livroEncontrado = livros.find((livro) => livro.id === Number(req.params.id));
+
+  if(livroEncontrado){
+    livroEncontrado.titulo = req.body.titulo;
+    livroEncontrado.autor = req.body.autor;
+    livroEncontrado.ano =  req.body.ano;
+    
+    res.json(livroEncontrado);
+  
+  }else{
+    res.send("Erro ao encontrar livro\n");
+  }
+  
+});
+
+app.delete('/livros/:id', async (req, res) => {
+  const indice = livros.findIndex((livro) => livro.id === Number(req.params.id));
+
+  if(indice != -1){
+    livros.splice(indice,1);
+    res.send("Livro apagado com sucesso\n");
+  }else{
+    res.send("Livro não encontrado ou não existe\n");
+  }
 
 });
 
